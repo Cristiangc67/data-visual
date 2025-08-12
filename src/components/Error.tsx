e00d7956afa9cbc2f0f9b5d04d1137cc2f92a9e8
Cristian Gutierrez <cristiangutierrezcruz67@gmail.com>
2025-08-12 05:21:42 -0300

    react import deleted in Error.tsx

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const Error = ({ errorText }: { errorText: string }) => {
   return (
     <div className=" bg-red-700/40 w-fit mx-auto px-5 rounded-2xl py-4 border border-red-400/50">
